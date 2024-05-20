7d2c21e05fcf9d4a7ec5baccc0e4da970d8f3977
bigwiggerz <bigwiggeroffical@gmail.com>
2024-05-21 02:15:59 +0300

    feat: realize user memory

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,5 +1,19 @@
-const getAll = async () =>
-  // TODO: mock implementation. should be replaced during task development
-  [];
+let users = [];
 
-export { getAll };
+export const getUsers = () => users;
+
+export const addUser = (user) => {
+  users.push(user);
+  return user;
+};
+
+export const findUserById = (id) => users.find(user => user.id === id);
+
+export const updateUser = (id, userData) => {
+  const user = findUserById(id);
+  if (user) {
+    Object.assign(user, userData);
+    return user;
+  }
+  return null;
+};
