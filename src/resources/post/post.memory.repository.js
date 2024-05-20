63abf453e4d9b8ccca86490abb29d74bee45b28d
bigwiggerz <bigwiggeroffical@gmail.com>
2024-05-21 02:17:57 +0300

    feat: realize post memory

---
+++ src/resources/post/post.memory.repository.js
@@ -0,0 +1,24 @@
+let posts = [];
+
+export const getPosts = () => posts;
+
+export const addPost = (post) => {
+    posts.push(post);
+    return post;
+  };
+
+export const findPostById = (id) => posts.find(post => post.id === id);
+
+export const updatePost = (id, postData) => {
+    const post = findPostById(id);
+    if (post) {
+      Object.assign(post, postData);
+      return post;
+    }
+    return null;
+};
+
+export const deletePost = (id) => {
+    posts = posts.filter(post => post.id !== id);
+    comments = comments.filter(comment => comment.postId !== id);
+  };
